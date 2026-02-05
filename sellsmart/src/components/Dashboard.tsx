5ba6365ab0ab969aaacf970f41e1125c1009b0ed
Harshit Singla <harshitashwani@gmail.com>
2026-02-05 14:18:56 +0530

    Adding more elements

--- sellsmart/src/components/Dashboard.tsx
+++ sellsmart/src/components/Dashboard.tsx
@@ -54,7 +54,7 @@ export const Simulator = () => {
     const fetchPrediction = async () => {
         setLoading(true);
         try {
-            const res = await fetch("http://localhost:8000/predict", {
+            const res = await fetch("http://127.0.0.1:8000/predict", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
